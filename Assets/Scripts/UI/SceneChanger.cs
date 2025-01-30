9babfec50b9009e687e43906e3282bb1fd148934
Jonathan Benz <98258254+JonathanBenz@users.noreply.github.com>
2025-01-29 20:17:24 -0500

    Added Scene Changing
    -Also imported Unity Input package and TMPro package.

---
+++ Assets/Scripts/UI/SceneChanger.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    /// <summary>
+    /// Load the next scene. If the current scene is the last scene, loop to the first scene. 
+    /// </summary>
+    public void GoToNextScene()
+    {
+        int totalSceneCount = SceneManager.sceneCountInBuildSettings;
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex >= totalSceneCount - 1) currentSceneIndex = -1;
+
+        SceneManager.LoadScene(++currentSceneIndex);
+    }
+}
