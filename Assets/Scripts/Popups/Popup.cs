b07c0452e9b07c8998c61331ac2dad8770413f49
Leo Schindler-Gerendasi (RIT Student) <ls1539@rit.edu>
2025-02-01 14:34:14 -0500

    Completed basic popup functionality
    When clicked on, the popup will display at its full size at the center of the screen. When clicked again, the popup will revert back to its original position.

--- Assets/Scripts/Popups/Popup.cs
+++ Assets/Scripts/Popups/Popup.cs
@@ -3,15 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// Very basic struct that stores all three components of a transform.
+/// Pretty much just makes it so that I don't have to make three seperate
+/// fields to store the reference to the popup's transform before being
+/// brought into focus.
+/// </summary>
+struct TransformComponents
+{
+    public Vector3 Pos { get; set; }
+    public Quaternion Rot { get; set; }
+    public Vector3 Scale { get; set; }
+
+    public void SetComponents(Vector3 pos, Quaternion rot, Vector3 scale)
+    {
+        Pos = pos;
+        Rot = rot;
+        Scale = scale;
+    }
+}
+
 public class Popup : MonoBehaviour
 {
     // ~~~ FIELDS ~~~
     // Whether or not the popup is in focus or not.
     private bool inFocus;
     // The transform of the popup before it was in-focus.
-    private Transform preFocusTransform;
-    // The collider of this popup.
+    private TransformComponents preFocusTransform;
+    // The collider attached to this popup.
     private Collider2D popupCollider;
+    // The SpriteRenderer attached to this popup.
+    private SpriteRenderer spriteRenderer;
 
     // ~~~ PROPERTIES ~~~
     /// <summary>
@@ -19,14 +41,16 @@ public class Popup : MonoBehaviour
     /// </summary>
     public Sprite Texture
     {
-        get { return GetComponent<SpriteRenderer>().sprite; }
-        set { GetComponent<SpriteRenderer>().sprite  = value; }
+        get { return spriteRenderer.sprite; }
+        set { spriteRenderer.sprite  = value; }
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        preFocusTransform = new TransformComponents();
         popupCollider = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -42,12 +66,42 @@ public class Popup : MonoBehaviour
             // Check to see if the mouse is hovering over the popup
             if (popupCollider.OverlapPoint(mousePos))
             {
-                GetComponent<SpriteRenderer>().color = Color.yellow;
+                // Highlighr the sprite green if it is being hovered over
+                if (spriteRenderer.color != Color.green)
+                    spriteRenderer.color = Color.green;
+
+                // When the mouse is clicked while this popup is being hovered over:
+                if (Mouse.current.leftButton.wasPressedThisFrame)
+                {
+                    // Mark the popup as in focus
+                    inFocus = true;
+                    spriteRenderer.color = Color.white;
+
+                    // Store references to the transform values from before it was in focus
+                    preFocusTransform.SetComponents(transform.position, transform.rotation, transform.localScale);
+
+                    // Make it display in the center of the screen
+                    transform.position = Vector2.zero;
+                    transform.rotation = Quaternion.Euler(0, 0, 0);
+                    transform.localScale = Vector2.one;
+                }
             }
-            else
+            else if (spriteRenderer.color != Color.white)
             {
-                GetComponent<SpriteRenderer>().color = Color.white;
+                spriteRenderer.color = Color.white;
             }
+        }
+        else
+        {
+            // When pressing the mouse button while this popup is in focus, un-focus it
+            // and return it to its original position
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                inFocus = false;
+                transform.position = preFocusTransform.Pos;
+                transform.rotation = preFocusTransform.Rot;
+                transform.localScale = preFocusTransform.Scale;
+            }    
         }
     }
 }
