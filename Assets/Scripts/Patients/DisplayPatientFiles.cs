e15b49e4acbf602b2fec49242e90639f1d63a790
Jonathan Benz <98258254+JonathanBenz@users.noreply.github.com>
2025-02-04 02:33:54 -0500

    Patient Assignment Complete

--- Assets/Scripts/Patients/DisplayPatientFiles.cs
+++ Assets/Scripts/Patients/DisplayPatientFiles.cs
@@ -67,16 +67,70 @@ public class DisplayPatientFiles : MonoBehaviour
         }
     }
     /// <summary>
-    /// Assign patient data
+    /// Assign patient data when the day starts. 
     /// </summary>
     /// <param name="patient"></param>
     public void Assign(SO_PatientFiles patient)
     {
+        this.gameObject.name = patient.FullName;
+
         // Add appropriate documents to be displayed here. 
         documents.Add(InsuranceClaim);
-        if (patient.HasGoldCard) documents.Add(GoldenInsuranceCard);
+        documents.Add(ClaimsUI);
         documents.Add(Background);
 
-        this.gameObject.name = patient.FullName;
+        // Filling out Insurance Claim text boxes
+        int i = 0;
+        fullName = ClaimTexts.transform.GetChild(i++).GetComponent<TextMeshProUGUI>();
+        fullName.text = patient.FullName;
+
+        dob = ClaimTexts.transform.GetChild(i++).GetComponent<TextMeshProUGUI>();
+        dob.text = patient.DOB;
+
+        maritalStatus = ClaimTexts.transform.GetChild(i++).GetComponent<TextMeshProUGUI>();
+        maritalStatus.text = patient.MaritalStatus;
+
+        address = ClaimTexts.transform.GetChild(i++).GetComponent<TextMeshProUGUI>();
+        address.text = patient.Address;
+
+        gender = ClaimTexts.transform.GetChild(i++).gameObject;
+        if (patient.Gender == Genders.Female) gender.transform.GetChild(0).gameObject.SetActive(true);
+        else if (patient.Gender == Genders.Female) gender.transform.GetChild(1).gameObject.SetActive(true);
+        else gender.transform.GetChild(2).gameObject.SetActive(true);
+
+        employment = ClaimTexts.transform.GetChild(i++).gameObject;
+        if (patient.IsEmployed) employment.transform.GetChild(0).gameObject.SetActive(true);
+        else employment.transform.GetChild(1).gameObject.SetActive(true);
+
+        dependants = ClaimTexts.transform.GetChild(i++).gameObject;
+        if (patient.HasDependents) dependants.transform.GetChild(0).gameObject.SetActive(true);
+        else dependants.transform.GetChild(1).gameObject.SetActive(true);
+
+        income = ClaimTexts.transform.GetChild(i++).GetComponent<TextMeshProUGUI>();
+        income.text = patient.Income;
+
+        preExistingConditions = ClaimTexts.transform.GetChild(i++).GetComponent<TextMeshProUGUI>();
+        preExistingConditions.text = patient.Conditions;
+
+        familyHealthHistory = ClaimTexts.transform.GetChild(i++).GetComponent<TextMeshProUGUI>();
+        familyHealthHistory.text = patient.FamilyHistory;
+
+        // If Patient has a gold card...
+        if (patient.HasGoldCard) { 
+            documents.Add(GoldenInsuranceCard); 
+            documents.Add(GoldCardUI);
+            i = 0;
+
+            gcFullName = GCTexts.transform.GetChild(i++).GetComponent<TextMeshProUGUI>();
+            gcFullName.text = patient.FullName;
+            gcDOB = GCTexts.transform.GetChild(i++).GetComponent<TextMeshProUGUI>();
+            gcDOB.text = patient.DOB;
+            gcAddress = GCTexts.transform.GetChild(i++).GetComponent<TextMeshProUGUI>();
+            gcAddress.text = patient.Address;
+            gcDateIssued = GCTexts.transform.GetChild(i++).GetComponent<TextMeshProUGUI>();
+            gcDateIssued.text = patient.GCIssueDate;
+            gcDateExpires = GCTexts.transform.GetChild(i++).GetComponent<TextMeshProUGUI>();
+            gcDateExpires.text = patient.GCExpirationDate;
+        }
     }
 }
