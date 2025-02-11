7ede80038fe36eaec9cf370d87dd0489f7126734
Jonathan Benz <98258254+JonathanBenz@users.noreply.github.com>
2025-02-11 11:16:50 -0500

    Last Minute Improvements

--- Assets/Scripts/UI/IntermissionUI.cs
+++ Assets/Scripts/UI/IntermissionUI.cs
@@ -9,13 +9,22 @@ public class IntermissionUI : MonoBehaviour
     [SerializeField] TextMeshProUGUI dayNumberText;
     [SerializeField] TextMeshProUGUI numInfractionsText;
     [SerializeField] TextMeshProUGUI moneyEarnedText;
+    [SerializeField] TextMeshProUGUI totalMoneyText;
     [SerializeField] Slider moneyJar;
 
     // Start is called before the first frame update
     void Start()
     {
         int dayNumber = FindObjectOfType<GameManager>().DayNum;
+        int numInfractions = FindObjectOfType<GameManager>().GetRulesBroken(dayNumber);
+        int moneyEarned = (300 - (numInfractions * 20));
+        int totalMoney = FindObjectOfType<GameManager>().money + moneyEarned;
         dayNumberText.text = $"Day {dayNumber + 1} Complete!";
-        numInfractionsText.text = $"Infractions: {FindObjectOfType<GameManager>().GetRulesBroken(dayNumber)}";
+        numInfractionsText.text = $"Infractions: {numInfractions}, -$20 deducted per infraction...";
+        moneyEarnedText.text = "Money Earned Today: $" + moneyEarned.ToString();
+        totalMoneyText.text = "Total Money: $" + totalMoney.ToString();
+        moneyJar.value = totalMoney / 750f;
+
+        FindObjectOfType<GameManager>().money += moneyEarned;
     }
 }
